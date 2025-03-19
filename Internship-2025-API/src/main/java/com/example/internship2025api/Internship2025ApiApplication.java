0ab23473ba958da6ce6f4e9b670d4cda150b37ac
Adi Ćefo <adi.cefo@edu.fit.ba>
2025-03-19 16:54:56 +0100

    First commit

---
+++ Internship-2025-API/src/main/java/com/example/internship2025api/Internship2025ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.internship2025api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Internship2025ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Internship2025ApiApplication.class, args);
+    }
+
+}
---
+++ Internship-2025-API/src/test/java/com/example/internship2025api/Internship2025ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internship2025api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Internship2025ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
